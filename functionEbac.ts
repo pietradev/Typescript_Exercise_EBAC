8bb0d53d8c07fa8b1be629f106511dc74ad48ea0
Pietra Oliveira <pietradev@gmail.com>
2025-01-03 07:10:18 -0500

    second  typescript exercise

---
+++ functionEbac.ts
@@ -0,0 +1,12 @@
+
+//creating functions
+function multiplicaN(n1: number, n2: number): number{
+    let result: number = 0;
+    result = n1 * n2;
+    return result;
+}
+
+
+function saudacao(name: string): string{
+    return `Ola ${name}`
+}
